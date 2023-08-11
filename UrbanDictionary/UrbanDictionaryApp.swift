d1bb72e4b10b5ebed64594f36d93beb0e1201e12
Alexander Chekel <alexandr.chekel@gmail.com>
2023-08-11 23:02:11 +0300

    Clean up todos

--- UrbanDictionary/UrbanDictionaryApp.swift
+++ UrbanDictionary/UrbanDictionaryApp.swift
@@ -7,9 +7,6 @@
 
 import SwiftUI
 
-// TODOs:
-// - Look into wrong definition urls. Defining "red apple" will result in empty screen.
-
 @main
 struct UrbanDictionaryApp: App {
     @Environment(\.viewFactory) private var viewFactory
