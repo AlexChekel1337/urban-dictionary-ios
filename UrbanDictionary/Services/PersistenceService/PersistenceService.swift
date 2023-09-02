fe5f9090bcb8e530106e6c1dd972d624a071b030
Alexander Chekel <alexandr.chekel@gmail.com>
2023-09-02 16:03:22 +0300

    Added support for Core Data and CloudKit

---
+++ UrbanDictionary/Services/PersistenceService/PersistenceService.swift
@@ -0,0 +1,24 @@
+//
+//  PersistenceService.swift
+//  UrbanDictionary
+//
+//  Created by Alexander Chekel on 02.09.2023.
+//
+
+import CoreData
+
+class PersistenceService {
+    static let shared = PersistenceService()
+
+    let container: NSPersistentCloudKitContainer
+
+    private init() {
+        container = NSPersistentCloudKitContainer(name: "UrbanDictionary")
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
---
+++ UrbanDictionary/Services/ServiceRegistry.swift
@@ -0,0 +1,28 @@
+//
+//  ServiceRegistry.swift
+//  UrbanDictionary
+//
+//  Created by Alexander Chekel on 02.09.2023.
+//
+
+import SwiftUI
+
+private struct UrbanDictionaryServiceEnvironmentKey: EnvironmentKey {
+    static let defaultValue = UrbanDictionaryService()
+}
+
+private struct PersistenceServiceEnvironmentKey: EnvironmentKey {
+    static let defaultValue = PersistenceService.shared
+}
+
+extension EnvironmentValues {
+    var urbanDictionaryService: UrbanDictionaryService {
+        get { self[UrbanDictionaryServiceEnvironmentKey.self] }
+        set { self[UrbanDictionaryServiceEnvironmentKey.self] = newValue }
+    }
+
+    var persistenceService: PersistenceService {
+        get { self[PersistenceServiceEnvironmentKey.self] }
+        set { self[PersistenceServiceEnvironmentKey.self] = newValue }
+    }
+}
